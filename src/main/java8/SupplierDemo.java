d734d9890a256cb9c998af73869650c20174741f
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-14 12:41:48 +0530

    SupplierDemo

---
+++ src/main/java8/SupplierDemo.java
@@ -0,0 +1,21 @@
+package src.main.java8;
+
+import java.util.function.Supplier;
+
+public class SupplierDemo implements Supplier<String> {
+    /**
+     * Gets a result.
+     *
+     * @return a result
+     */
+    @Override
+    public String get() {
+        return "Hello from Supplier";
+    }
+
+    public static void main(String[] args) {
+        Supplier<String> supplier = new SupplierDemo();
+
+        System.out.println(supplier.get());
+    }
+}
---
+++ src/main/java8/SupplierMain.java
@@ -0,0 +1,20 @@
+package src.main.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierMain {
+
+    public static void main(String[] args) {
+
+        Supplier<String> supplier = ()-> "Hello World";
+        System.out.println(supplier.get());
+
+        List<String> list = Arrays.asList("Abc", "Xyz");
+        List<String> list2 = Arrays.asList();
+       // System.out.println(Stream.of("India", "Australia").findAny().orElseGet(supplier));
+        System.out.println(list.stream().findAny().orElseGet(supplier));
+        System.out.println(list2.stream().findAny().orElseGet(supplier));
+    }
+}
