6d0435d40dbffd11feb1dd0a03ca145d1dc58271
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-20 17:37:44 +0530

    Using lambda for executors

--- src/Mutlithreading/BankExample.java
+++ src/Mutlithreading/BankExample.java
@@ -2,8 +2,14 @@ package src.Mutlithreading;
 
 import src.Mutlithreading.ThreadLifecylce.Bank;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 public class BankExample {
     public static void main(String[] args) {
+
+        final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
 
         //Intrinsic Locking - Synchronized
         //Bank  bankAccount = new BankAccountSynchronized();
@@ -18,7 +24,11 @@ public class BankExample {
             }
         };
 
-        Thread t1 = new Thread(task, "MyThread_01");
+        ExecutorService executors = Executors.newFixedThreadPool(4);
+
+        IntStream.range(0, 4).forEach(i -> executors.submit(task));
+
+        /*Thread t1 = new Thread(task, "MyThread_01");
         Thread t2 = new Thread(task, "MyThread_02");
         Thread t3 = new Thread(task, "MyThread_03");
         Thread t4 = new Thread(task, "MyThread_04");
@@ -26,6 +36,6 @@ public class BankExample {
         t1.start();
         t2.start();
         t3.start();
-        t4.start();
+        t4.start();*/
     }
 }
