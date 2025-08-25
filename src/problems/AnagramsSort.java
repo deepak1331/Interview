5ab99593625a5b17bc97fd984184d5ad7ea8e900
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-25 15:04:25 +0530

    Added Anagrams.java

---
+++ src/problems/AnagramsSort.java
@@ -0,0 +1,41 @@
+package src.problems;
+
+import java.util.*;
+
+public class AnagramsSort {
+
+    public static void main(String[] args) {
+        String[] arr = {"act", "god", "cat", "dog", "tac"};
+
+        List<List<String>> res = anagrams(arr);
+        System.out.println(res);
+    }
+
+    private static List<List<String>> anagrams(String[] arr) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        if (arr != null) {
+            for (String s : arr) {
+                String sortedStr = sortedWord(s);
+                if (!map.containsKey(sortedStr)) {
+                    map.put(sortedStr, result.size());
+                    result.add(new ArrayList<>());
+                }
+                result.get(map.get(sortedStr)).add(s);
+            }
+        }
+        return result;
+    }
+
+    public static String sortedWord(String str) {
+        if (str != null) {
+            char[] chars = str.toLowerCase().toCharArray();
+            Arrays.sort(chars);
+            //str = Arrays.toString(chars);
+            //System.out.println("Sorted word: " + str);
+            return Arrays.toString(chars);
+        }
+        return null;
+    }
+}
