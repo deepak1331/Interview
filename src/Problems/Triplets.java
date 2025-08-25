ab821fe1f624b677037025f09b1f5900260d998a
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-25 16:33:08 +0530

    Refactored Triplets.java

--- src/Problems/Triplets.java
+++ src/Problems/Triplets.java
@@ -14,25 +14,23 @@ public class Triplets {
 
     public static void main(String[] args) {
 
-        int[] input = new int[]{-1,1,2,3,-3,5,-2};
-        //List<Integer> inputList = List.of(-1, 1, 2, 3, -3, 5, -2);
+        int[] input = new int[]{-1, 1, 2, 3, -3, 5, -2};
         ArrayList<ArrayList<Integer>> outputList = new ArrayList<>();
         int sum = 0;
-        for(int i =0;i<input.length;i++){
-            for(int j=i;j<input.length;j++){
-                for(int k=j;k<input.length;k++){
-                    if(input[i]+input[j]+input[k] == 0)
-                    {
-                        ArrayList<Integer> output =  new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i; j < input.length; j++) {
+                for (int k = j; k < input.length; k++) {
+                    if (input[i] + input[j] + input[k] == 0) {
+                        ArrayList<Integer> output = new ArrayList<>();
                         output.add(input[i]);
                         output.add(input[k]);
                         output.add(input[j]);
                         outputList.add(output);
-                       // inputList.remove(output);
                     }
                 }
             }
         }
-        System.out.println(outputList);
+        System.out.println("Triplets combinations with sum ZERO (a + b + c = 0) are as follows: ");
+        outputList.forEach(System.out::println);
     }
 }
